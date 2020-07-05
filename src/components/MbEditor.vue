a523a7eae57263f4862590a531e45e284db2ae12
Amadeus Stadler <amadeus.stadler@gmail.com>
2020-07-05 22:42:38 +0200

    Bootstrap the editor component

---
+++ src/components/MbEditor.vue
@@ -0,0 +1,44 @@
+<template lang="html">
+  <div class="editor">
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    dark: Boolean,
+    disabled: Boolean,
+    error: String,
+    formats: {
+      type: Array,
+      default: () => [
+        'blockquote',
+        'bold',
+        'code',
+        'code-block',
+        'header',
+        'image',
+        'indent',
+        'italic',
+        'linebreak',
+        'link',
+        'list',
+        'strike',
+        'underline',
+      ],
+    },
+    label: String,
+    outputFormat: {
+      type: String,
+      default: 'text',
+      validator: (v) => ['text', 'markdown', 'html'].includes(v),
+    },
+    placeholder: String,
+    value: String,
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+</style>
