1290d4fc053df203a092c100f85502692718f7f3
Amadeus Stadler <amadeus.stadler@gmail.com>
2020-11-23 20:39:49 +0100

    Add some guards gainst malformed html

--- src/assets/js/formatHTML.js
+++ src/assets/js/formatHTML.js
@@ -1,10 +1,15 @@
 // Adapted from here https://stackoverflow.com/questions/3913355/how-to-format-tidy-beautify-in-javascript
 export default function formatHTML(html) {
+  if (!html.startsWith('<') || !html.endsWith('>')) return html;
+
   const tab = '  ';
   let result = '';
   let indent = '';
+  const nodes = html.split(/>\s*</);
 
-  html.split(/>\s*</).forEach((element) => {
+  if (nodes.length < 2) return html;
+
+  nodes.forEach((element) => {
     if (element.match(/^\/\w/)) indent = indent.substring(tab.length);
 
     result += `${indent}<${element}>\n`;
