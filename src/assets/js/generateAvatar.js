2973992809c60e1aa1084a0f69d31fc60dadfbc2
Amadeus Stadler <amadeus.stadler@gmail.com>
2020-12-21 13:00:30 +0100

    Make generated avatars more interesting

--- src/assets/js/generateAvatar.js
+++ src/assets/js/generateAvatar.js
@@ -1,3 +1,7 @@
+function between(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 function fgColor(color, force) {
   // calculate text color based on background
   // adapted from https://awik.io/determine-color-bright-dark-using-javascript/
@@ -16,9 +20,25 @@ function fgColor(color, force) {
   return '#ffffff';
 }
 
-export default (initials, color, textColor) => {
-  const fg = fgColor(color, textColor);
-  const bg = color;
+function rotatedGradient(ctx, x1, y1, x2, y2, index) {
+  const points = ['tl', 'tr', 'bl', 'br'];
+  const from = index ? points[index] : points[Math.floor(Math.random() * points.length)];
+
+  switch (from) {
+    case 'tl':
+      return ctx.createLinearGradient(x1, y1, x2, y2);
+    case 'tr':
+      return ctx.createLinearGradient(x2, y1, x1, y2);
+    case 'bl':
+      return ctx.createLinearGradient(x1, y2, x2, y1);
+    case 'br':
+      return ctx.createLinearGradient(x2, y2, x1, y1);
+    default:
+      return ctx.createLinearGradient(x1, y1, x2, y2);
+  }
+}
+
+export default (initials, color, color2, textColor) => {
   const weight = '700';
   const fontFamily = '"Inter", sans-serif';
   const size = 128;
@@ -28,6 +48,14 @@ export default (initials, color, textColor) => {
   const fs = size / 2.5;
   const c = document.createElement('canvas');
   const ctx = c.getContext('2d');
+  const fg = fgColor(color, textColor);
+
+  const bg = ctx.createLinearGradient(0, 0, size, size);
+  bg.addColorStop(0, color);
+  bg.addColorStop(1, color2);
+
+  const elements = ['square', 'diamond', 'circle'];
+  const element = elements[Math.floor(Math.random() * elements.length)];
 
   c.width = size;
   c.height = size;
@@ -37,6 +65,48 @@ export default (initials, color, textColor) => {
   ctx.rect(0, 0, c.width, c.height);
   ctx.fillStyle = bg;
   ctx.fill();
+
+  ctx.lineWidth = 8;
+
+  for (let i = 0; i < 3; i += 1) {
+    ctx.beginPath();
+    const radius = between(40, 96) + 8 * i;
+    const elX = between(-radius / 2, size - radius / 2) + 8 * i;
+    const elY = between(-radius / 2, size - radius / 2) + 8 * i;
+
+    if (element === 'square') {
+      const elementGradient = rotatedGradient(ctx, elX, elY, elX + radius, elY + radius, i);
+      elementGradient.addColorStop(0, 'rgba(255,255,255,0.2)');
+      elementGradient.addColorStop(1, 'rgba(255,255,255,0)');
+
+      ctx.strokeStyle = elementGradient;
+      ctx.rect(elX, elY, radius, radius);
+      ctx.stroke();
+    } else if (element === 'diamond') {
+      ctx.save();
+
+      ctx.translate(size / 2, size / 2);
+      ctx.rotate(Math.PI / 4);
+      ctx.translate(elX, elY);
+
+      const elementGradient = rotatedGradient(ctx, -radius / 2, -radius / 2, -radius / 2 + radius, -radius / 2 + radius, i);
+      elementGradient.addColorStop(0, 'rgba(255,255,255,0.2)');
+      elementGradient.addColorStop(1, 'rgba(255,255,255,0)');
+      ctx.strokeStyle = elementGradient;
+
+      ctx.rect(-radius / 2, -radius / 2, radius, radius);
+      ctx.stroke();
+      ctx.restore();
+    } else {
+      const elementGradient = rotatedGradient(ctx, elX - radius / 2, elY - radius / 2, elX + radius / 2, elY + radius / 2, i);
+      elementGradient.addColorStop(0, 'rgba(255,255,255,0.2)');
+      elementGradient.addColorStop(1, 'rgba(255,255,255,0)');
+      ctx.strokeStyle = elementGradient;
+
+      ctx.ellipse(elX, elY, radius / 2, radius / 2, 0, 0, 360);
+      ctx.stroke();
+    }
+  }
 
   ctx.font = `${weight} ${fs}px ${fontFamily}`;
   ctx.textAlign = 'center';
