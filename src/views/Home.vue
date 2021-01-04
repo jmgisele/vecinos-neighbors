37f66faf02ef96919304e1ffc8ddaf4a0b89a8c6
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-01-04 04:17:41 +0100

    Add more scaffolding

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,12 @@
     </header>
     <main>
       <MbButton :dark="dark" type="primary" @click="$store.commit('addToast', { message: `Toast ${tc} lorem ipsum dolor sicet numquam dolor ipsut`, timeout: false }); tc++">Add toast</MbButton>
+      <button class="add-project" :class="{dark}">
+        <div class="icon-wrapper">
+          <MbIcon icon="download" />
+        </div>
+        <span>Import Project</span>
+      </button>
     </main>
   </div>
 </template>
@@ -48,6 +54,7 @@ export default {
 <style lang="stylus" scoped>
 @require '../assets/styles/breakpoints'
 @require '../assets/styles/colors'
+@require '../assets/styles/corners'
 
 .home
   &.dark
@@ -76,8 +83,59 @@ export default {
     overflow-x: hidden
     overflow-y: auto
     padding: 2rem
+    display: grid
+    grid-template-columns: repeat(auto-fill, (320 / 16)rem)
+    grid-auto-rows: (246 / 16)rem
+    grid-gap: 2rem
+    justify-content: center
 
     @media $mobile
       height: "calc(100vh - %s)" % (144 / 16)rem
       padding: 1rem
+
+    .add-project
+      background-color: $bg
+      border: none
+      color: inherit
+      padding: 2rem
+      display: flex
+      align-items: center
+      justify-content: center
+      flex-direction: column
+      border-radius: $radius-m
+      box-shadow: inset 0 0 0 0.0625rem $accent
+      cursor: pointer
+      transition: background-color 200ms ease
+
+      &.dark
+        background-color: $bg-tertiary-dark
+
+        &:focus,
+        &:hover
+          background-color: $bg-secondary-dark
+
+        &:active
+          background-color: $bg-dark
+
+      &:focus,
+      &:hover
+        background-color: $bg-secondary
+
+      &:focus
+        box-shadow: inset 0 0 0 0.125rem $accent
+
+      &:active
+        background-color: $bg-tertiary
+        transform: translateY(2px)
+
+      .icon-wrapper
+        padding: 1rem
+        border-radius: 50%
+        background-color: $accent-secondary
+        margin-bottom: 1rem
+        color: $text-dark
+
+        .icon
+          width: 2rem
+          height: @width
 </style>
