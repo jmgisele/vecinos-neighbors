f61c8675497c28a19ea7a6394284d210146fde2b
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-01-11 12:26:30 +0100

    Update documentation

--- src/views/Components.vue
+++ src/views/Components.vue
@@ -338,7 +338,7 @@
       <section v-else-if="activeTabValue === 'file-lists'" class="tab file-lists">
         <h3>File Lists</h3>
         <p>This component is a fully fledged file browser that can display the contents of a folder and offers the ability for custom actions on the files.</p>
-        <MbFileList :dark="dark" :file-actions="[{ action: showFileToast, icon: 'folder-open', label: 'Open' }, { disabled: true, icon: 'arrow-right', label: 'Move' }, { action: showFileToast, icon: 'trash', label: 'Delete', type: 'negative' }]" :folders-first="true" :folders-only="false" root="/" show-hidden :action="{ callback: () => $store.commit('addToast', { message: 'Hi there!'}), label: 'Add', icon: 'plus', type: 'positive'}" @fileclick="showFileToast" />
+        <MbFileList :dark="dark" :file-actions="[{ action: showFileToast, icon: 'folder-open', label: 'Open', foldersOnly: true }, { disabled: true, icon: 'arrow-right', label: 'Move' }, { action: showFileToast, icon: 'trash', label: 'Delete', type: 'negative', filesOnly: true }]" :folders-first="true" :folders-only="false" root="/" show-hidden :action="{ callback: () => $store.commit('addToast', { message: 'Hi there!'}), label: 'Add', icon: 'plus', type: 'positive'}" @fileclick="showFileToast" />
         <h3>Props</h3>
         <MbTable :data="props.fileLists" />
         <h3>Events</h3>
@@ -475,9 +475,9 @@ export default {
         ],
         fileLists: [
           ['Name', 'Type', 'Default', 'Notes'],
-          ['`action`', 'Object', '', 'Takes Button Props and a Label and Callback, will be shown at the top right as a primary action such as "create new"'],
+          ['`action`', 'Object', '', 'Takes Button Props and a Label and Callback, will be shown at the top right as a primary action such as "create new" and "move here" (called with currentPath)'],
           ['`dark`', 'Boolean', '`false`', ''],
-          ['`fileActions`', 'Array', '`[]`', 'Takes objects with Context-Menu options including an action, which will be called with the file’s path'],
+          ['`fileActions`', 'Array', '`[]`', 'Takes objects with Context-Menu options including an action, which will be called with the file’s path. If filesOnly / foldersOnly are specified for an action, it will only show up in the respective type’s menu'],
           ['`filterable`', 'Boolean', '`true`', ''],
           ['`foldersFirst`', 'Boolean', '`true`', ''],
           ['`foldersOnly`', 'Boolean', '`false`', ''],
@@ -488,6 +488,7 @@ export default {
         icons: [
           ['Name', 'Type', 'Default'],
           ['`icon`', 'String', "`'mattrbld'`"],
+          ['`noTransition`', 'Boolean', '`false`'],
         ],
         inputs: [
           ['Name', 'Type', 'Default'],
