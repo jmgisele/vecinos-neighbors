4132d092577edc7125b81491ce834b256d8d3f78
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-03-15 23:34:07 +0100

    Add simple rimraf

--- src/fs/index.js
+++ src/fs/index.js
@@ -1,4 +1,5 @@
 import FS from '@isomorphic-git/lightning-fs';
+import { join } from '@isomorphic-git/lightning-fs/src/path';
 
 const fs = new FS('mattrfs');
 
@@ -10,5 +11,25 @@ function joinPath(...parts) { // taken from https://github.com/isomorphic-git/li
   return path;
 }
 
+async function deleteFileOrFolder(path) {
+  const stat = await fs.promises.lstat(path);
+
+  if (stat.isDirectory()) return rmrf(path); // eslint-disable-line no-use-before-define
+  return fs.promises.unlink(path);
+}
+
+async function rmrf(path) {
+  if (!path || typeof path !== 'string' || !path.trim()) throw new Error('Invalid directory');
+  if (path.trim() === '/') throw new Error('You may not delete the root directory');
+
+  // assume the folder is not empty since we would’ve used rmdir then
+  const entities = await fs.promises.readdir(path);
+  await Promise.all(entities.map((entity) => {
+    const fullPath = join(path, entity);
+    return deleteFileOrFolder(fullPath);
+  }));
+  await fs.promises.rmdir(path);
+}
+
 export default fs.promises;
-export { fs as PlainFS, joinPath };
+export { fs as PlainFS, joinPath, rmrf };
