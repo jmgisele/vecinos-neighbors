666a9c1e3bff4d13ddf6b543074c6bc9fedcd39e
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-03-20 23:33:10 +0100

    Add a worker for rimraffing

---
+++ src/fs/worker.js
@@ -0,0 +1,34 @@
+import FS from '@isomorphic-git/lightning-fs';
+import { join } from '@isomorphic-git/lightning-fs/src/path';
+import MagicPortal from '../assets/js/FixedMagicPortal';
+
+const fs = new FS('mattrfs');
+const portal = new MagicPortal(self); // eslint-disable-line no-restricted-globals
+
+async function deleteFileOrFolder(path) {
+  const stat = await fs.promises.lstat(path);
+
+  if (stat.isDirectory()) return rmrf(path); // eslint-disable-line no-use-before-define
+  return fs.promises.unlink(path);
+}
+
+async function rmrf(path) {
+  if (!path || typeof path !== 'string' || !path.trim()) throw new Error('Invalid directory');
+  if (path.trim() === '/') throw new Error('You may not delete the root directory');
+
+  try {
+    const entities = await fs.promises.readdir(path);
+    await Promise.all(entities.map((entity) => {
+      const fullPath = join(path, entity);
+      return deleteFileOrFolder(fullPath);
+    }));
+    await fs.promises.rmdir(path);
+  } catch (err) {
+    if (err.code === 'ENOTDIR') await fs.promises.unlink(path); // it must be a file
+    else throw err;
+  }
+}
+
+portal.set('workerThread', {
+  rmrf,
+});
---
+++ src/fs/workerFS.js
@@ -0,0 +1,13 @@
+import FsWorker from 'worker-loader!./worker'; // eslint-disable-line
+
+import MagicPortal from '../assets/js/FixedMagicPortal';
+
+const worker = new FsWorker();
+const portal = new MagicPortal(worker);
+
+async function rmrf(path) {
+  const workerThread = await portal.get('workerThread');
+  return workerThread.rmrf(path);
+}
+
+export { rmrf }; // eslint-disable-line import/prefer-default-export
