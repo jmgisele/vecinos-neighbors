f5f0242768cfa4dee22041540c865a404fa1e9c7
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-03-23 17:20:15 +0100

    Add media library scaffold

---
+++ src/views/ProjectMediaLibrary.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div class="project-dashboard">
+    <h2>Media Library</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    dark: Boolean,
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+</style>
