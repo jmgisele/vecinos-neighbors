d7d1623dbab51f38bf7b687bcf0f8d3e83e195f2
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-03-30 18:08:12 +0200

    Remove project properties that are file lists

--- src/store/index.js
+++ src/store/index.js
@@ -4,18 +4,15 @@ import fs from '../fs';
 import observers from './observers';
 
 const persistentAppProperties = ['activeUser', 'corsProxy', 'initialised', 'locallyChangedFiles'];
-const persistentProjectProperties = ['corsProxy', 'id', 'name', 'sidebar', 'slugifyOptions', 'collections', 'customFields', 'customRoles', 'schemas'];
+const persistentProjectProperties = ['corsProxy', 'id', 'name', 'sidebar', 'slugifyOptions', 'customRoles'];
 const persistentUserProperties = ['email', 'id', 'name', 'projects', 'role', 'theme', 'uiScale'];
 
 const projectDefaults = {
   avatar: null,
-  collections: [],
   corsProxy: null,
-  customFields: [],
   customRoles: [],
   id: null,
   name: null,
-  schemas: [],
   sidebar: [],
   slugifyOptions: null,
   users: [],
