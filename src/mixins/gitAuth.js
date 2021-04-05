d21de4030391e142371ce17c4aad844b6809a833
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-04-05 18:19:58 +0200

    Refactor to have authentication tools as a separate mixin

---
+++ src/mixins/gitAuth.js
@@ -0,0 +1,48 @@
+import GitLoginModal from '../components/utility/GitLoginModal.vue';
+
+export default {
+  components: {
+    GitLoginModal,
+  },
+  data() {
+    return {
+      credentialPromise: null,
+      credentials: null,
+      gitLoginMessage: `This repository seems to be private. Please log into your <strong>${this.gitProvider}</strong> account to confirm that you may perform this action.`,
+      showGitLoginModal: false,
+    };
+  },
+  methods: {
+    async onGitAuth() {
+      if (this.credentials && this.credentials.user && this.credentials.password) return { username: this.credentials.user, password: this.credentials.password };
+      if (this.$store.state.user.gitAuth) {
+        const { user, password } = this.$store.state.user.gitAuth;
+        return { username: user, password };
+      }
+      this.gitLoginMessage = `This repository seems to be private. Please log into your <strong>${this.gitProvider || 'Git'}</strong> account to confirm that you may perform this action.`;
+      this.credentials = await this.openGitLoginModal();
+      this.showGitLoginModal = false;
+      if (this.credentials === 'cancel') return { cancel: true };
+      return { username: this.credentials.user, password: this.credentials.password };
+    },
+    async onGitAuthFailure() {
+      if (this.$store.state.user.gitAuth) this.$store.commit('setUserProperty', { key: 'gitAuth', value: null });
+      this.gitLoginMessage = 'Sorry, that didn’t work. This might mean that you don’t have access to this repository, or that you typed the wrong username / password combination. Please try again.';
+      this.credentials = await this.openGitLoginModal();
+      this.showGitLoginModal = false;
+
+      if (this.credentials === 'cancel') return { cancel: true };
+      return { username: this.credentials.user, password: this.credentials.password };
+    },
+    async onGitAuthSuccess() {
+      if (this.credentials.savePassword) {
+        // WARNING: This might be insecure considering XSS attacks (then again, if there’s a XSS, we probably are screwed anyway)
+        this.$store.commit('setUserProperty', { key: 'gitAuth', value: { password: this.credentials.password, user: this.credentials.user } });
+      }
+    },
+    openGitLoginModal() {
+      this.showGitLoginModal = true;
+      return new Promise((resolve) => { this.credentialPromise = resolve; });
+    },
+  },
+};
--- src/mixins/gitTools.js
+++ src/mixins/gitTools.js
@@ -1,10 +1,8 @@
 import TimeoutError from '../assets/js/TimeoutError';
-import GitLoginModal from '../components/utility/GitLoginModal.vue';
+
+import gitAuth from './gitAuth';
 
 export default {
-  components: {
-    GitLoginModal,
-  },
   computed: {
     cloneLabel() {
       if (!this.cloneStep) return 'Initialising';
@@ -17,10 +15,6 @@ export default {
     return {
       cloneProgress: 0,
       cloneStep: '',
-      credentialPromise: null,
-      credentials: null,
-      gitLoginMessage: `This repository seems to be private. Please log into your <strong>${this.gitProvider}</strong> account to confirm that you may perform this action.`,
-      showGitLoginModal: false,
     };
   },
   methods: {
@@ -43,41 +37,11 @@ export default {
         this.$store.commit('addToast', { message: `Something went wrong while fetching branches: ${err.message}`, type: 'error' });
       }
     },
-    async onGitAuth() {
-      if (this.credentials && this.credentials.user && this.credentials.password) return { username: this.credentials.user, password: this.credentials.password };
-      if (this.$store.state.user.gitAuth) {
-        const { user, password } = this.$store.state.user.gitAuth;
-        return { username: user, password };
-      }
-      this.gitLoginMessage = `This repository seems to be private. Please log into your <strong>${this.gitProvider || 'Git'}</strong> account to confirm that you may perform this action.`;
-      this.credentials = await this.openGitLoginModal();
-      this.showGitLoginModal = false;
-      if (this.credentials === 'cancel') return { cancel: true };
-      return { username: this.credentials.user, password: this.credentials.password };
-    },
-    async onGitAuthFailure() {
-      if (this.$store.state.user.gitAuth) this.$store.commit('setUserProperty', { key: 'gitAuth', value: null });
-      this.gitLoginMessage = 'Sorry, that didn’t work. This might mean that you don’t have access to this repository, or that you typed the wrong username / password combination. Please try again.';
-      this.credentials = await this.openGitLoginModal();
-      this.showGitLoginModal = false;
-
-      if (this.credentials === 'cancel') return { cancel: true };
-      return { username: this.credentials.user, password: this.credentials.password };
-    },
-    async onGitAuthSuccess() {
-      if (this.credentials.savePassword) {
-        // WARNING: This might be insecure considering XSS attacks (then again, if there’s a XSS, we probably are screwed anyway)
-        this.$store.commit('setUserProperty', { key: 'gitAuth', value: { password: this.credentials.password, user: this.credentials.user } });
-      }
-    },
     async onGitProgress(progress) {
       this.cloneStep = progress.phase;
       if (progress.total) this.cloneProgress = progress.loaded / progress.total;
       else this.cloneProgress = 0;
     },
-    openGitLoginModal() {
-      this.showGitLoginModal = true;
-      return new Promise((resolve) => { this.credentialPromise = resolve; });
-    },
   },
+  mixins: [gitAuth],
 };
