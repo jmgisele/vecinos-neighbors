8936da40ec092e2a977661c7492cfc6cd70bb6fe
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-04-12 22:27:10 +0200

    Simplify recursive update function

--- src/views/settings/SchemaSettings.vue
+++ src/views/settings/SchemaSettings.vue
@@ -123,7 +123,7 @@ export default {
     async updateLocallyChangedFiles(path) {
       const entities = await fs.readdir(path);
       return Promise.all(entities.map((entity) => {
-        if (entity.endsWith('.json')) return new Promise((res) => res(this.$store.commit('addLocallyChangedFile', joinPath(path, entity))));
+        if (entity.endsWith('.json')) return this.$store.commit('addLocallyChangedFile', joinPath(path, entity));
         return this.updateLocallyChangedFiles(joinPath(path, entity));
       }));
     },
