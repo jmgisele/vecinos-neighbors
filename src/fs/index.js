26b8dd5e8df324c616b386d79b4d46b4c5d1bd2c
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-04-13 23:25:57 +0200

    Add mkdirp

--- src/fs/index.js
+++ src/fs/index.js
@@ -10,6 +10,41 @@ async function deleteFileOrFolder(path) {
   return fs.promises.unlink(path);
 }
 
+// Adapted from https://github.com/isomorphic-git/isomorphic-git/blob/main/src/models/FileSystem.js
+async function exists(filepath) {
+  try {
+    await fs.promises.stat(filepath);
+    return true;
+  } catch (err) {
+    if (err.code === 'ENOENT' || err.code === 'ENOTDIR') return false;
+    throw err;
+  }
+}
+
+// Adapted from https://github.com/isomorphic-git/isomorphic-git/blob/main/src/models/FileSystem.js
+async function mkdirp(filepath, _selfCall = false) {
+  try {
+    await fs.promises.mkdir(filepath);
+    return;
+  } catch (err) {
+    // If err is null then operation succeeded!
+    if (err === null) return;
+    // If the directory already exists, that's OK!
+    if (err.code === 'EEXIST') return;
+    // Avoid infinite loops of failure
+    if (_selfCall) throw err;
+    // If we got a "no such file or directory error" backup and try again.
+    if (err.code === 'ENOENT') {
+      const parent = dirname(filepath);
+      // Check to see if we've gone too far
+      if (parent === '.' || parent === '/' || parent === filepath) throw err;
+      // Infinite recursion, what could go wrong?
+      await mkdirp(parent);
+      await mkdirp(filepath, true);
+    }
+  }
+}
+
 async function rmrf(path) {
   if (!path || typeof path !== 'string' || !path.trim()) throw new Error('Invalid directory');
   if (path.trim() === '/') throw new Error('You may not delete the root directory');
@@ -27,17 +62,6 @@ async function rmrf(path) {
   }
 }
 
-// Adapted from https://github.com/isomorphic-git/isomorphic-git/blob/main/src/models/FileSystem.js
-async function exists(filepath) {
-  try {
-    await fs.promises.stat(filepath);
-    return true;
-  } catch (err) {
-    if (err.code === 'ENOENT' || err.code === 'ENOTDIR') return false;
-    throw err;
-  }
-}
-
 export default fs.promises;
 export {
   basename as pathBasename,
@@ -45,5 +69,6 @@ export {
   exists,
   fs as PlainFS,
   join as joinPath,
+  mkdirp,
   rmrf,
 };
