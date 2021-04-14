3d49439e001fb433bde5dc13e2a2cf605e60cc33
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-04-14 18:37:11 +0200

    Finalise field spec

--- src/data/default-fields.js
+++ src/data/default-fields.js
@@ -2,18 +2,27 @@
 
 interface Field {
   type: String!,
+  default: Any,
+  description: String,
+  group: String,
   icon: String,
-  name: String!,
-  options: {
-    default: Any,
-    group: String,
-    localised: Boolean,
-    validation: { required: Boolean },
-    visibility: { limitToGroups: Array, showByValue: { field: String, value: Any } },
-    // + field specific options
-  },
+  key: String!,
+  label: String!,
+  localised: Boolean,
+  options: [
+    {
+      component: String!,
+      key: String!,
+      props: Object,
+      slot: String,
+      value: Any,
+    },
+  ],
   tab: String,
+  validation: { max: Number, min: Number, regex: String, required: Boolean },
   value: Any,
+  visibility: { hidden: false, limitToGroups: Array, showByValue: { field: String, value: Any } },
+  visualOnly: Boolean!, // excluded from content files, serves purely visual function
 },
 
 */
@@ -21,31 +30,42 @@ interface Field {
 export default [
   {
     type: 'id',
+    default: null,
+    description: 'Adds a unique ID to the content',
+    group: 'basic',
     icon: 'hashtag',
-    name: '',
-    options: {
-      default: null,
-      group: 'basic',
-      localised: false,
-      validation: { required: false },
-      visibility: { limitToGroups: [], showByValue: { field: null, value: null } },
-      // + field specific options
-    },
+    key: '',
+    label: 'Unique ID',
+    options: [
+      {
+        component: 'MbRadioGroup',
+        key: 'type',
+        props: { inline: true, options: [{ label: 'Filepath', value: 'filepath' }, { label: 'Generated UUID', value: 'uuid' }] },
+        value: 'filepath',
+      },
+      {
+        component: 'MbToggle',
+        key: 'editable',
+        slot: 'Make editable',
+        value: false,
+      },
+    ],
     tab: null,
+    validation: { required: true },
     value: null,
+    visibility: { hidden: false, limitToGroups: [], showByValue: { field: null, value: null } },
   },
   {
     type: 'languages',
+    default: null,
+    description: 'Allows selecting which languages the content will be available in',
+    group: 'basic',
     icon: 'language',
-    name: '',
-    options: {
-      default: null,
-      group: 'basic',
-      localised: false,
-      validation: { required: false },
-      visibility: { limitToGroups: [], showByValue: { field: null, value: null } },
-    },
+    key: '',
+    label: 'Content Languages',
     tab: null,
+    validation: { min: 1 },
     value: null,
+    visibility: { hidden: false, limitToGroups: [], showByValue: { field: null, value: null } },
   },
 ];
