5b7927d3301ddefc591b2da677696bdf0a5147b9
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-04-18 01:09:34 +0200

    Allow nested lists

--- src/components/utility/FieldArrangementItem.vue
+++ src/components/utility/FieldArrangementItem.vue
@@ -1,25 +1,33 @@
 <template lang="html">
-  <div class="field-arrangement-item" :class="{ dark, 'drag-active': $store.state.application.dragActive }">
-    <MbIcon class="drag-handle" icon="drag-handle" />
-    <div class="field-icon">
-      <MbIcon :icon="icon" />
+  <div class="field-arrangement-item" :class="{ 'drag-active': $store.state.application.dragActive }">
+    <div class="info" :class="{ dark }">
+      <MbIcon class="drag-handle" icon="drag-handle" />
+      <div class="field-icon">
+        <MbIcon :icon="icon" />
+      </div>
+      <span class="label"><strong>{{label}}</strong></span>
+      <span v-if="localised" class="chip">Localised</span>
+      <span v-if="required" class="chip">Required</span>
+      <span class="type">{{type}} field</span>
+      <MbIcon v-if="hidden" class="hidden" icon="hide" />
+      <MbIcon class="action" :icon="active ? 'cross' : 'pencil'" />
     </div>
-    <span class="label"><strong>{{label}}</strong></span>
-    <span v-if="localised" class="chip">Localised</span>
-    <span v-if="required" class="chip">Required</span>
-    <span class="type">{{type}} field</span>
-    <MbIcon v-if="hidden" class="hidden" icon="hide" />
-    <MbIcon class="action" :icon="active ? 'cross' : 'pencil'" />
+    <FieldArrangementList v-if="nestedFields" :dark="dark" :fields="nestedFields" :parent-key="`${parentKey}.${fieldKey}`" />
   </div>
 </template>
 
 <script>
+import { defineAsyncComponent } from 'vue';
+
 export default {
+  name: 'FieldArrangementItem',
+  components: {
+    FieldArrangementList: defineAsyncComponent(() => import('./FieldArrangementList.vue')),
+  },
   data() {
     return {
     };
   },
-  emits: ['visibility-change'],
   props: {
     active: Boolean,
     dark: Boolean,
@@ -34,6 +42,7 @@ export default {
       default: 'Unlabled field',
     },
     localised: Boolean,
+    nestedFields: Array,
     parentKey: String,
     required: Boolean,
     type: {
@@ -50,90 +59,96 @@ export default {
 @require '../../assets/styles/corners'
 
 .field-arrangement-item
-  display: flex
-  align-items: center
-  padding: 1rem
-  background-color: $bg-secondary
-  border-radius: $radius-l
-  cursor: pointer
-  transition: background-color 200ms ease
+    &.drag-active
+      > *:not(.field-arrangement-list)
+        pointer-events: none
 
-  &:hover
-    background-color: $bg
-
-  &.drag-active
-    > *
-      pointer-events: none
-
-  &.dark
-    background-color: $bg-secondary-dark
+  .info
+    display: flex
+    align-items: center
+    padding: 1rem
+    background-color: $bg-secondary
+    border-radius: $radius-l
+    cursor: pointer
+    transition: background-color 200ms ease
 
     &:hover
-      background-color: $bg-tertiary-dark
+      background-color: $bg
 
-    span
-      &.chip
-        background-color: $bg-dark
+    &.dark
+      background-color: $bg-secondary-dark
 
-      &.type
-        color: $text-secondary-dark
+      &:hover
+        background-color: $bg-tertiary-dark
 
-    .hidden
-      color: $text-tertiary-dark
+      span
+        &.chip
+          background-color: $bg-dark
 
-  .drag-handle
-    margin-right: 1rem
-    cursor: move
-    flex-shrink: 0
+        &.type
+          color: $text-secondary-dark
 
-  .field-icon
-    padding: 0.5rem
-    background-color: $accent
-    color: $text-dark
-    border-radius: $radius-m
-    margin-right: 1rem
+      .hidden
+        color: $text-tertiary-dark
 
-    .icon
-      display: block
+    .drag-handle
+      margin-right: 1rem
+      cursor: move
+      flex-shrink: 0
 
-  span
-    white-space: nowrap
-    overflow: hidden
-    text-overflow: ellipsis
-
-    &.label
+    .field-icon
+      padding: 0.5rem
+      background-color: $accent
+      color: $text-dark
+      border-radius: $radius-m
       margin-right: 1rem
 
-      @media $tablet
-        margin-right: auto
+      .icon
+        display: block
 
-    &.chip
-      padding: (4 / 16)rem (12 / 16)rem
-      background-color: $bg
-      margin-right: 0.5rem
-      border-radius: 1rem
+    span
+      white-space: nowrap
+      overflow: hidden
+      text-overflow: ellipsis
 
-      @media $tablet
+      &.label
+        margin-right: 1rem
+
+        @media $tablet
+          margin-right: auto
+
+      &.chip
+        padding: (4 / 16)rem (12 / 16)rem
+        background-color: $bg
+        margin-right: 0.5rem
+        border-radius: 1rem
+
+        @media $tablet
+          display: none
+
+      &.type
+        text-transform: capitalize
+        margin-left: auto
+        margin-right: 0.5rem
+        color: $text-secondary
+
+        @media $tablet
+          display: none
+
+    .hidden
+      flex-shrink: 0
+      color: $text-tertiary
+      margin: 0 0.5rem
+
+    .action
+      margin: 0 0.5rem
+      flex-shrink: 0
+
+      @media $mobile
         display: none
 
-    &.type
-      text-transform: capitalize
-      margin-left: auto
-      margin-right: 0.5rem
-      color: $text-secondary
-
-      @media $tablet
-        display: none
-
-  .hidden
-    flex-shrink: 0
-    color: $text-tertiary
-    margin: 0 0.5rem
-
-  .action
-    margin: 0 0.5rem
-    flex-shrink: 0
-
-    @media $mobile
-      display: none
+  .field-arrangement-list
+    margin-top: 1rem
+    margin-right: 0.125rem
+    margin-left: 2rem
 </style>
