c04b28c599b98443a7c22aae365a3343701c6d12
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-04-24 19:40:01 +0200

    Add autoscroll support

--- src/components/MbSortableList.vue
+++ src/components/MbSortableList.vue
@@ -7,6 +7,10 @@
 </template>
 
 <script>
+import findClosestScrollParent from '../assets/js/findClosestScrollParent';
+
+import autoscroll from '../mixins/autoscroll';
+
 export default {
   beforeUnmount() {
     window.removeEventListener('pointerup', this.stopDrag);
@@ -21,6 +25,10 @@ export default {
       dragging: false,
       draggingClone: null,
       lastEl: null,
+      scrollParent: {
+        el: null,
+        rect: null,
+      },
       wasBottomHalf: null,
     };
   },
@@ -33,6 +41,8 @@ export default {
       this.beingDragged = true; // we moved the cursor
       this.draggingClone.style.left = `${e.clientX - this.cloneClickDelta.x}px`;
       this.draggingClone.style.top = `${e.clientY - this.cloneClickDelta.y}px`;
+
+      this.autoscroll(this.scrollParent.el, this.scrollParent.rect, e.clientX, e.clientY);
 
       const el = document.elementFromPoint(e.clientX, e.clientY);
       if (!el || !el.dataset.index || el.dataset.area !== this.areaId || el === this.dragging) return;
@@ -54,6 +64,7 @@ export default {
       if (this.draggingClone) this.destroyClone();
       this.dragging = e.currentTarget;
       this.activeItem = this.items[index];
+
       const rect = e.currentTarget.getBoundingClientRect();
       const clone = e.currentTarget.cloneNode(true);
       this.cloneClickDelta = { x: e.clientX - rect.left, y: e.clientY - rect.top };
@@ -66,10 +77,21 @@ export default {
       clone.style.margin = 0;
       document.body.append(clone);
       this.draggingClone = clone;
+
       const style = document.createElement('STYLE');
       style.innerText = '* { cursor: grabbing !important; }';
       style.id = `${this.areaId}-grabbingStyle`;
       document.querySelector('head').append(style);
+
+      this.scrollParent.el = findClosestScrollParent(this.$el);
+      const parentRect = this.scrollParent.el.getBoundingClientRect();
+      this.scrollParent.rect = {
+        top: Math.max(parentRect.top, 0),
+        left: Math.max(parentRect.left, 0),
+        width: Math.min(parentRect.width, window.innerWidth),
+        height: Math.min(parentRect.height, window.innerHeight),
+      };
+
       window.addEventListener('pointerup', this.stopDrag);
       window.addEventListener('pointermove', this.handlePointerMove, { passive: true });
     },
@@ -87,6 +109,7 @@ export default {
       window.removeEventListener('pointerup', this.stopDrag);
       window.removeEventListener('pointermove', this.handlePointerMove, { passive: true });
       document.getElementById(`${this.areaId}-grabbingStyle`).remove();
+      if (this.autoscrollAnimationFrame) window.cancelAnimationFrame(this.autoscrollAnimationFrame);
       const targetRect = this.dragging.getBoundingClientRect();
       const { left: currentLeft, top: currentTop } = this.draggingClone.style;
       if (Number.parseInt(currentLeft, 10) === Math.floor(targetRect.left) && Number.parseInt(currentTop, 10) === Math.floor(targetRect.top)) {
@@ -99,6 +122,7 @@ export default {
       this.draggingClone.addEventListener('transitionend', this.destroyClone, { once: true });
     },
   },
+  mixins: [autoscroll],
   props: {
     items: Array,
     keyName: String,
