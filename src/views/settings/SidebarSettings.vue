2e1140fae9fbd076bb0978c1676b199352cdd6fe
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-05-07 13:01:29 +0200

    Fix index detection

--- src/views/settings/SidebarSettings.vue
+++ src/views/settings/SidebarSettings.vue
@@ -177,7 +177,7 @@ export default {
 
       if (isEqual(modifiedEntry, this.entryBeingModified)) return;
 
-      if (entryIndex) shallowCopy.splice(entryIndex, 1, modifiedEntry);
+      if (typeof entryIndex !== 'undefined') shallowCopy.splice(entryIndex, 1, modifiedEntry);
       this.entryBeingModified = modifiedEntry;
       this.sidebarOptions = shallowCopy;
     },
