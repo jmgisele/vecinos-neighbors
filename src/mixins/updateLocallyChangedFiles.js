af6e75af47344ae3586879851c16487be36efb67
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-05-13 13:11:49 +0200

    Make local changes independent from file ending

--- src/mixins/updateLocallyChangedFiles.js
+++ src/mixins/updateLocallyChangedFiles.js
@@ -4,8 +4,9 @@ export default {
   methods: {
     async updateLocallyChangedFiles(path) {
       const entities = await fs.readdir(path);
-      return Promise.all(entities.map((entity) => {
-        if (entity.endsWith('.json')) return this.$store.commit('addLocallyChangedFile', joinPath(path, entity));
+      const stats = await Promise.all(entities.map((entity) => fs.stat(joinPath(path, entity))));
+      return Promise.all(entities.map((entity, index) => {
+        if (stats[index].isFile()) return this.$store.commit('addLocallyChangedFile', joinPath(path, entity));
         return this.updateLocallyChangedFiles(joinPath(path, entity));
       }));
     },
