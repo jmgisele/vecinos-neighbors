1148e77f709d7afcec8d8d0ab6bb8ab75778650e
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-05-16 12:59:04 +0200

    Handle localised fields

--- src/components/fields/TextField.vue
+++ src/components/fields/TextField.vue
@@ -1,7 +1,25 @@
 <template lang="html">
   <section class="text field" :class="{ dark, 'in-split': inSplit }">
-    <MbEditor v-if="options && (options.wrapping || options.multiline)" :allow-new-lines="options && options.multiline" :dark="dark" :error="error" :label="label" :max-len="(validation && validation.max) || null" :model-value="modelValue" @update:model-value="handleInput" />
-    <MbInput v-else :dark="dark" :error="error" :label="label" :max-len="(validation && validation.max) || null" :model-value="modelValue" @update:model-value="handleInput" />
+    <template v-if="!localised || !languages || languages.length < 2">
+      <MbEditor v-if="options && (options.wrapping || options.multiline)" :allow-new-lines="options && options.multiline" :dark="dark" :error="error" :label="label" :max-len="(validation && validation.max) || null" :model-value="modelValue" @update:model-value="handleInput" />
+      <MbInput v-else :dark="dark" :error="error" :label="label" :max-len="(validation && validation.max) || null" :model-value="modelValue" @update:model-value="handleInput" />
+    </template>
+    <div v-else class="field-group" @click="showModal = true">
+      <div class="left">
+        <span class="label">{{label}}</span>
+        <span class="content">{{modelValue && modelValue[languages[0]]}}</span>
+      </div>
+      <teleport :disabled="!teleportTarget" :to="teleportTarget">
+        <template v-for="lang in languages" :key="lang">
+          <MbEditor v-if="options && (options.wrapping || options.multiline)" :allow-new-lines="options && options.multiline" :dark="dark" :error="error && error[lang]" :label="lang" :max-len="(validation && validation.max) || null" :model-value="modelValue && modelValue[lang]" @update:model-value="handleInput($event, lang)" />
+          <MbInput v-else :dark="dark" :error="error && error[lang]" :label="lang" :max-len="(validation && validation.max) || null" :model-value="modelValue && modelValue[lang]" @update:model-value="handleInput($event, lang)" />
+        </template>
+      </teleport>
+      <MbIcon icon="pencil" />
+      <MbModal :dark="dark" :title="label" :visible="showModal" @close="showModal = false">
+        <div class="modal-body" ref="modalBody" />
+      </MbModal>
+    </div>
   </section>
 </template>
 
@@ -11,13 +29,32 @@ import userInputToRegex from '../../assets/js/userInputToRegex';
 import field from '../../mixins/field';
 
 export default {
+  computed: {
+    teleportTarget() {
+      if (!this.inSplit && this.splitTarget) return this.splitTarget;
+      return this.modalBody;
+    },
+  },
+  data() {
+    return {
+      modalBody: null,
+      showModal: false,
+    };
+  },
   methods: {
-    handleInput(newValue) {
-      this.validate(newValue);
-      this.$emit('update:modelValue', newValue);
+    handleInput(newValue, lang) {
+      const error = this.validate(newValue);
+      if (!lang) {
+        if (error || this.error) this.$emit('update:error', error); // we only emit if we have an error set or the value is invalid
+
+        this.$emit('update:modelValue', newValue);
+      } else {
+        if (error || (this.error && this.error[lang])) this.$emit('update:error', { ...this.error, [lang]: error });
+        this.$emit('update:modelValue', { ...this.modelValue, [lang]: newValue });
+      }
     },
     validate(value) {
-      if (!this.validation) return;
+      if (!this.validation) return '';
 
       let error = '';
 
@@ -33,10 +70,13 @@ export default {
         }
       }
 
-      if (error || this.error) this.$emit('update:error', error); // we only emit if we have an error set or the value is invalid
+      return error;
     },
   },
   mixins: [field],
+  mounted() {
+    if (this.$refs.modalBody) this.modalBody = this.$refs.modalBody;
+  },
 };
 </script>
 
@@ -44,5 +84,16 @@ export default {
 @require '../../assets/styles/colors'
 @require '../../assets/styles/corners'
 
-// .text.field
+.text.field
+  &.in-split.dark
+    .input,
+    .editor::v-deep(.content-wrapper)
+      background-color: $bg-tertiary-dark
+
+  .input
+    margin-top: 0
+    width: 100%
+
+  .editor::v-deep(.content-wrapper)
+    margin-top: 0
 </style>
