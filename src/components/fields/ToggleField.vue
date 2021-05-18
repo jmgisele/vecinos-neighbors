c1421532f87a613cf99aebb8ca1b62da344c6bec
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-05-18 10:37:21 +0200

    Add toggle field

---
+++ src/components/fields/ToggleField.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <section class="toggle field">
+    <MbToggle :dark="dark" :icons="icons" :model-value="modelValue" @update:model-value="$emit('update:modelValue', $event)">
+      {{label}}:
+    </MbToggle>
+  </section>
+</template>
+
+<script>
+import field from '../../mixins/field';
+
+export default {
+  computed: {
+    icons() {
+      if (this.options.iconLeft && this.options.iconRight) return [this.options.iconLeft, this.options.iconRight];
+      return null;
+    },
+  },
+  mixins: [field],
+};
+</script>
+
+<style lang="stylus" scoped>
+</style>
