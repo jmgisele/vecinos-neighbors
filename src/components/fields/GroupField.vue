835d5f80e48de97a9053394b36492b835f9c1634
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-05-19 19:02:47 +0200

    Add field group

---
+++ src/components/fields/GroupField.vue
@@ -0,0 +1,70 @@
+<template lang="html">
+  <section class="group field">
+    <div class="display-wrapper" :class="{ active, dark, error, 'in-split': inSplit, 'no-display-value': !localisedDisplayValue }" tabindex="0" @click="openGroup" @keyup.enter.space="openGroup" @keydown.space.prevent>
+      <div class="left">
+        <p class="label" :class="{ unstyled: !localisedDisplayValue }">{{error ? 'One or more subfields have errors' : label}}</p>
+        <p v-if="localisedDisplayValue" class="content">{{localisedDisplayValue}}</p>
+      </div>
+      <MbIcon :icon="error ? 'error' : active ? 'cross' : 'pencil'" />
+    </div>
+    <MbModal class="group-content" :dark="dark" :title="label" :visible="showModal" @close="closeGroup" @keyup.ctrl.enter="closeGroup">
+      <teleport v-if="!teleportTarget || active" :disabled="!teleportTarget" :to="teleportTarget">
+        <MbFieldsEditor compact :dark="dark" :fields="children" in-split :model-value="modelValue" :parent-languages="languages" @update:model-value="update" />
+      </teleport>
+      <template #actions>
+        <MbButton :dark="dark" type="primary" @click="closeGroup">Done</MbButton>
+      </template>
+    </MbModal>
+  </section>
+</template>
+
+<script>
+import { get as _get } from 'lodash-es';
+
+import field from '../../mixins/field';
+
+export default {
+  computed: {
+    localisedDisplayValue() {
+      if (!this.displayField) return null;
+      const displayValue = _get(this.modelValue, this.displayField);
+
+      if (displayValue !== null && typeof displayValue === 'object') return Object.values(displayValue)[0];
+      return displayValue;
+    },
+  },
+  data() {
+    return {
+      showModal: false,
+    };
+  },
+  methods: {
+    closeGroup() {
+      if (this.splitTarget) this.$emit('update:active', false);
+      else this.showModal = false;
+    },
+    openGroup() {
+      if (this.active) {
+        this.closeGroup();
+        return;
+      }
+      if (this.splitTarget) this.$emit('update:active', true);
+      else this.showModal = true;
+    },
+    update(v) {
+      this.$emit('update:modelValue', v);
+    },
+  },
+  mixins: [field],
+};
+</script>
+
+<style lang="stylus" scoped>
+@require '../../assets/styles/fields'
+
+.group.field
+  .display-wrapper
+    &.no-display-value // to match height of input fields
+      padding-top: 1.0625rem
+      padding-bottom: @padding-top
+</style>
