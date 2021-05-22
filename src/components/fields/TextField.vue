be90eef56445e2a0b96064d2c0bc78808ba06742
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-05-22 10:59:57 +0200

    Don’t convert non existing errors to string

--- src/components/fields/TextField.vue
+++ src/components/fields/TextField.vue
@@ -1,8 +1,8 @@
 <template lang="html">
   <section class="text field" :class="{ dark, localised: showLocalisedOptions }">
     <template v-if="!showLocalisedOptions">
-      <MbEditor v-if="options && (options.wrapping || options.multiline)" :allow-new-lines="options && options.multiline" :class="{ 'in-split': inSplit }" :dark="dark" :error="String(error)" :label="label" :max-len="(validation && validation.max) || null" :model-value="safeModelValue" ref="editor" @update:model-value="handleInput" />
-      <MbInput v-else :class="{ 'in-split': inSplit }" :dark="dark" :error="String(error)" :label="label" :max-len="(validation && validation.max) || null" :model-value="safeModelValue" @update:model-value="handleInput" />
+      <MbEditor v-if="options && (options.wrapping || options.multiline)" :allow-new-lines="options && options.multiline" :class="{ 'in-split': inSplit }" :dark="dark" :error="error && String(error)" :label="label" :max-len="(validation && validation.max) || null" :model-value="safeModelValue" ref="editor" @update:model-value="handleInput" />
+      <MbInput v-else :class="{ 'in-split': inSplit }" :dark="dark" :error="error && String(error)" :label="label" :max-len="(validation && validation.max) || null" :model-value="safeModelValue" @update:model-value="handleInput" />
     </template>
     <LocalisedFieldsContainer
       v-else
