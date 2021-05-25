ddd9ab8b53e56302b8de95a3a2b550700af4d7d5
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-05-25 11:05:58 +0200

    Fix potential infinite loop

--- src/assets/js/findClosestScrollParent.js
+++ src/assets/js/findClosestScrollParent.js
@@ -1,5 +1,7 @@
 // This function is based on https://stackoverflow.com/a/42543908
 export default function getScrollParent(element, includeHidden) {
+  if (!element) return document.scrollingElement;
+
   let style = getComputedStyle(element);
   const excludeStaticParent = style.position === 'absolute';
   const overflowRegex = includeHidden ? /(auto|scroll|hidden)/ : /(auto|scroll)/;
@@ -8,7 +10,11 @@ export default function getScrollParent(element, includeHidden) {
   let parent = element;
   while (parent) {
     style = getComputedStyle(parent);
-    if (excludeStaticParent && style.position === 'static') continue; // eslint-disable-line no-continue
+    if (excludeStaticParent && style.position === 'static') {
+      parent = parent.parentElement;
+      continue; // eslint-disable-line no-continue
+    }
+
     if (overflowRegex.test(style.overflow + style.overflowY + style.overflowX) && parent.clientHeight && parent.clientHeight < parent.scrollHeight) return parent;
     parent = parent.parentElement;
   }
