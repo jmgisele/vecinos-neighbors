411b6b5d45b80579cbd8d5a53b489b1d3506f203
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-06-04 22:39:20 +0200

    Add basic structure and styling

--- src/views/ProjectDashboard.vue
+++ src/views/ProjectDashboard.vue
@@ -1,11 +1,20 @@
 <template lang="html">
   <div class="project-dashboard">
-    <h2>Dashboard</h2>
+    <header>
+      <h1>Welcome back, {{firstName}}!</h1>
+    </header>
+    <section class="wrapper">
+    </section>
   </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    firstName() {
+      return this.$store.getters.userInCurrentProject.name.split(' ')[0];
+    },
+  },
   props: {
     dark: Boolean,
   },
@@ -13,4 +22,32 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+@require '../assets/styles/breakpoints'
+
+.project-dashboard
+  padding: 0 2rem 8rem 2rem
+
+  @media $mobile
+    padding-left: 1rem
+    padding-right: 1rem
+    padding-bottom: 4rem
+
+  header,
+  section.wrapper
+    max-width: (1120 / 16)rem
+    margin: 0 auto
+
+  header
+    margin-bottom: 4rem
+
+    h1
+      margin-top: 1.5rem
+      line-height: 2.5rem
+
+  section.wrapper
+    &:not(:last-child)
+      margin-bottom: 8rem
+
+      @media $mobile
+        margin-bottom: 4rem
 </style>
