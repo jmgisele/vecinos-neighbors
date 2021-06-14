b7c2ad4c5f5591cf8b0cd5b99458945372fe25df
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-06-14 23:17:13 +0200

    Add news section

--- src/views/ProjectDashboard.vue
+++ src/views/ProjectDashboard.vue
@@ -19,8 +19,28 @@
         </div>
       </MbScroller>
     </section>
-    <section class="wrapper">
+    <section class="wrapper news">
       <h2>News and Announcements</h2>
+      <transition mode="out-in">
+        <MbLoader v-if="newsLoading" />
+        <div v-else>
+          <section v-for="(article, index) in sortedNews" class="news-section" :class="[article.type, { dark }]" :key="index">
+            <template v-if="article.type === 'changelog'">
+              <p>You’re running version {{changelog.version}} of Mattrbld. <a href="#" @click.prevent="openChangelog">See what’s new</a></p>
+              <footer>
+                <span>{{article.author}}, {{article.formattedDate}}</span>
+              </footer>
+            </template>
+            <template v-else>
+              <h3 v-if="article.title">{{article.title}}</h3>
+              <p>{{article.blurb}} <a v-if="article.renderedContent" href="#" @click.prevent="activeNews = article; showDetails = true">Read more</a></p>
+              <footer>
+                <span>{{article.author}}, {{article.formattedDate}}</span>
+              </footer>
+            </template>
+          </section>
+        </div>
+      </transition>
     </section>
     <section class="wrapper commits">
       <h2>Recent Updates</h2>
@@ -41,19 +61,33 @@
         <li v-for="(path, index) in locallyChangedFiles" class="change-indicator" :class="{ dark }" :key="index">
           <span class="path">{{path.replace(`${projectDir}/`, '')}}</span>
         </li>
+        <li v-if="locallyChangedFiles.length === 0" class="empty-state" :class="{ dark }">Your local changes will appear here once you edit some content</li>
       </ul>
     </section>
+    <MbModal class="details-modal" :dark="dark" :title="activeNews.type === 'changelog' ? 'Changelog' : activeNews.title" :visible="showDetails" @close="showDetails = false">
+      <article v-html="activeNews.type === 'changelog' ? renderedChangelog : activeNews.renderedContent" />
+      <template #actions>
+        <MbButton :dark="dark" @click="showDetails = false">Close</MbButton>
+      </template>
+    </MbModal>
   </div>
 </template>
 
 <script>
 import { formatDistanceToNow } from 'date-fns';
 import { log } from 'isomorphic-git';
+import * as matter from 'gray-matter';
+
+import CHANGELOG from 'raw-loader!../../CHANGELOG.md'; // eslint-disable-line import/no-webpack-loader-syntax, import/no-unresolved
+
 import fs, { joinPath, PlainFS } from '../fs';
+import MarkdownParser from '../assets/js/MarkdownParser';
 
 import generateAvatar from '../assets/js/generateAvatar';
 
 import AsyncImage from '../components/utility/AsyncImage.vue';
+
+const md = new MarkdownParser();
 
 export default {
   beforeUnmount() {
@@ -65,6 +99,10 @@ export default {
     AsyncImage,
   },
   computed: {
+    changelog() {
+      const { data, content } = matter(CHANGELOG);
+      return { content, date: new Date(data.updatedAt), version: data.currentVersion };
+    },
     firstName() {
       if (!this.$store.getters.userInCurrentProject) return 'Anonymous'; // Dashboard gets unloaded after Project so this is needed to avoid an error
       return this.$store.getters.userInCurrentProject.name.split(' ')[0];
@@ -79,14 +117,30 @@ export default {
       const { sidebar } = this.$store.state.currentProject;
       return sidebar.filter((entry) => entry.showInDashboard && entry.target);
     },
+    sortedNews() {
+      if (this.newsLoading) return [];
+      const news = [
+        ...this.news,
+        {
+          author: 'Mattrbld', createdAt: this.changelog.date, formattedDate: formatDistanceToNow(this.changelog.date, { addSuffix: true }), type: 'changelog',
+        },
+      ];
+      return news.sort((a, b) => b.createdAt - a.createdAt);
+    },
   },
   created() {
+    this.fetchNews();
     this.fetchLog();
   },
   data() {
     return {
+      activeNews: {},
       log: [],
       logLoading: true,
+      news: [],
+      newsLoading: true,
+      renderedChangelog: null,
+      showDetails: false,
     };
   },
   emits: ['push'],
@@ -143,6 +197,34 @@ export default {
         return cleanCommit;
       });
       this.logLoading = false;
+    },
+    async fetchNews() {
+      try {
+        const newsPath = joinPath(this.projectDir, '.mattrbld', 'news');
+        const newsFiles = await fs.readdir(newsPath);
+        const rawNews = await Promise.all(newsFiles.map((name) => fs.readFile(joinPath(newsPath, name), 'utf8')));
+
+        rawNews.forEach((item) => {
+          const { data, content } = matter(item);
+          this.news.push({
+            author: data.author,
+            blurb: data.blurb,
+            createdAt: new Date(data.createdAt),
+            formattedDate: formatDistanceToNow(new Date(data.createdAt), { addSuffix: true }),
+            renderedContent: md.parse(content),
+            title: data.title,
+            type: 'article',
+          });
+        });
+      } catch (err) {
+        if (err.code !== 'ENOENT') this.$store.commit('addToast', { message: `Something went wrong while fetching the news: ${err.message}`, type: 'error' });
+      }
+      this.newsLoading = false;
+    },
+    openChangelog() {
+      if (this.renderedChangelog === null) this.renderedChangelog = md.parse(this.changelog.content);
+      this.activeNews = this.sortedNews.find((news) => news.type === 'changelog');
+      this.showDetails = true;
     },
   },
   props: {
@@ -228,6 +310,37 @@ export default {
         .button
           width: 100%
 
+    &.news
+      .loader,
+      > div
+        &.v-enter-active,
+        &.v-leave-active
+          transition: opacity 200ms ease
+
+          &.v-enter-from,
+          &.v-leave-to
+            opacity: 0
+
+      .news-section
+        &.dark
+          > footer
+            color: $text-secondary-dark
+
+        &:not(:last-child)
+          margin-bottom: 3rem
+
+        > h3
+          margin-bottom: 0.5rem
+
+        > p
+          margin-top: 0.5rem
+          margin-bottom: 0.5rem
+          max-width: 40rem
+
+        > footer
+          font-size: (14 / 16)rem
+          color: $text-secondary
+
     &.commits,
     &.local-changes
       .loader,
@@ -267,6 +380,14 @@ export default {
             margin-right: 1rem
             flex-shrink: 0
 
+          &.empty-state
+            border: none
+            color: $text-secondary
+            justify-content: center
+
+            &.dark
+              color: $text-secondary-dark
+
           &:not(:last-child)
             margin-bottom: 1rem
 
@@ -298,4 +419,13 @@ export default {
 
               @media $mobile
                 display: none
+
+.details-modal article
+  &::v-deep(> *:first-child)
+    margin-top: 0
+
+  &::v-deep(img)
+    display: block
+    max-width: 100%
+    margin: 2rem auto
 </style>
