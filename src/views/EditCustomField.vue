44ed67254bb9d43dfacccc60b2d52454cc15bca5
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-06-22 23:29:40 +0200

    Add settings and related functions

--- src/views/EditCustomField.vue
+++ src/views/EditCustomField.vue
@@ -11,13 +11,30 @@
       </div>
     </header>
     <SchemaFieldsEditor v-model="customField" :active-tab="0" :dark="dark" :project-id="$route.params.id" :tabs="[]" @update:modelValue="checkForChanges" />
+    <MbModal class="edit-custom-field-modal" :dark="dark" slim title="Custom Field Settings" :visible="showCustomFieldSettings" @after-close="resetCustomFieldName" @close="showCustomFieldSettings = false">
+      <MbInput v-model.lazy="newCustomFieldName" :dark="dark" :error="nameError" icon="document" label="Name" @blur="validateName" />
+      <MbInput v-model.lazy="group" :dark="dark" icon="folder" label="Category" />
+      <MbEditor v-model="description" :allow-new-lines="false" :dark="dark" label="Description" />
+      <div class="icon-picker-wrapper">
+        <span>Custom Field icon:</span>
+        <MbIconPicker v-model="icon" :dark="dark" />
+      </div>
+      <MbHighlightBox color="negative" :dark="dark" label="Danger Zone">
+        <p>Deleting a Custom Field will not remove it from Schemas that are referring to it. It will only prevent the addition of new fields of this type.</p>
+        <MbButton :dark="dark" icon="trash" type="negative" @click="deleteCustomField">Delete Custom Field</MbButton>
+      </MbHighlightBox>
+      <template #actions>
+        <MbButton :dark="dark" @click="showCustomFieldSettings = false">Cancel</MbButton>
+        <MbButton :dark="dark" :disabled="Boolean(nameError)" type="primary" @click="renameCustomField">Save</MbButton>
+      </template>
+    </MbModal>
   </div>
 </template>
 
 <script>
 import { cloneDeep, isEqual } from 'lodash-es';
 import { status } from 'isomorphic-git';
-// import slugify from '@sindresorhus/slugify';
+import slugify from '@sindresorhus/slugify';
 
 import fs, { exists, PlainFS, joinPath, pathBasename, pathDirname } from '../fs'; // eslint-disable-line object-curly-newline, no-unused-vars
 import flattenFields from '../assets/js/flattenFields';
@@ -61,6 +78,9 @@ export default {
       return next((vm) => {
         // needs to be an array so we can pass it to the fields editor
         vm.customField = [customField]; // eslint-disable-line no-param-reassign
+        vm.description = customField.description; // eslint-disable-line no-param-reassign
+        vm.group = customField.group; // eslint-disable-line no-param-reassign
+        vm.icon = customField.icon; // eslint-disable-line no-param-reassign
         vm.fileStatus = fileStatus; // eslint-disable-line no-param-reassign
         vm.newCustomFieldName = prettifyEntityName(pathBasename(path)); // eslint-disable-line no-param-reassign
         if (fromBackup) vm.checkForChanges([customField]); // eslint-disable-line no-param-reassign
@@ -101,6 +121,7 @@ export default {
   },
   computed: {
     customFieldName() {
+      if (!this.$route.params.path) return '';
       return prettifyEntityName(pathBasename(this.$route.params.path));
     },
     isMobile() {
@@ -121,8 +142,12 @@ export default {
   data() {
     return {
       customField: [],
+      description: '',
       fileStatus: null,
       forceNavigation: false,
+      group: '',
+      icon: '',
+      nameError: '',
       newCustomFieldName: '',
       originalField: null,
       saveLoading: false,
@@ -133,7 +158,78 @@ export default {
   methods: {
     async checkForChanges(nv) {
       if (!this.originalField) this.originalField = JSON.parse(await fs.readFile(this.$route.params.path, 'utf8'));
-      this.wasChanged = !isEqual(this.originalField, nv[0]); // we’re only interested in the first field
+      this.wasChanged = !isEqual(this.originalField, nv[0]) || this.group !== this.originalField.group || this.description !== this.originalField.description || this.icon !== this.originalField.icon; // we’re only interested in the first field
+    },
+    deleteCustomField() {
+      const { id, path } = this.$route.params;
+
+      this.showCustomFieldSettings = false;
+      if (this.wasChanged) {
+        const clone = cloneDeep(this.customField[0]);
+        clone.description = this.description;
+        clone.group = this.group;
+        clone.icon = this.icon;
+        this.$store.commit('setAppProperty', { key: 'temporaryCustomFieldStorage', value: clone });
+      }
+      this.forceNavigation = true;
+      this.$store.commit('addToSoftDeleted', path);
+      this.$store.commit('addToast', {
+        action: () => {
+          this.$store.commit('removeFromSoftDeleted', path);
+          this.$router.replace({ name: 'Edit Custom Field', params: { id, path } });
+        },
+        actionLabel: 'Undo',
+        message: `The custom field “${this.customFieldName}” was deleted`,
+        onClose: async (undone) => {
+          if (undone) return;
+
+          try {
+            await fs.unlink(path);
+            this.$store.commit('addLocallyChangedFile', path);
+            this.$store.dispatch('saveAppData');
+          } catch (err) {
+            this.$store.commit('addToast', { message: `Something went wrong while deleting the Custom Field: ${err.message}`, type: 'error' });
+            this.$router.replace({ name: 'Edit Custom Field', params: { id, path } });
+          } finally {
+            this.$store.commit('removeFromSoftDeleted', path);
+            this.$store.commit('setAppProperty', { key: 'temporaryCustomFieldStorage', value: null });
+          }
+        },
+        timeout: 5000,
+        type: 'warning',
+      });
+      this.$router.replace({ name: 'Project.Settings', params: { id }, query: { tab: 'custom-fields' } });
+    },
+    async renameCustomField() {
+      if (this.icon !== this.customField[0].icon) this.customField[0].icon = this.icon; // this is here so there’s an immediate change, the other properties get set during saveChanges
+
+      this.checkForChanges(this.customField);
+      if (this.newCustomFieldName === this.customFieldName) {
+        this.showCustomFieldSettings = false;
+        return;
+      }
+      this.validateName();
+      if (this.nameError) return;
+
+      const newName = slugify(this.newCustomFieldName, this.$store.state.currentProject.slugifyOptions || { lowercase: false, decamelize: false, preserveLeadingUnderscore: true });
+      const newPath = joinPath(pathDirname(this.$route.params.path), `${newName}.json`);
+      const alreadyExists = await exists(newPath);
+
+      if (alreadyExists) {
+        this.nameError = 'A Custom Field with this name already exists';
+        return;
+      }
+
+      await fs.rename(this.$route.params.path, newPath);
+      this.$store.commit('addLocallyChangedFile', this.$route.params.path);
+      this.$store.commit('addLocallyChangedFile', newPath);
+      this.showCustomFieldSettings = false;
+      this.forceNavigation = true;
+      this.$router.replace({ params: { id: this.$route.params.id, path: newPath } });
+    },
+    resetCustomFieldName() {
+      this.newCustomFieldName = this.customFieldName;
+      this.nameError = '';
     },
     async saveChanges() {
       if (this.customField.length > 1) {
@@ -152,6 +248,9 @@ export default {
           const newField = cloneDeep(this.customField[0]);
           if (!newField.version || typeof newField.version !== 'number') newField.version = 1;
           else newField.version += 1;
+          newField.description = this.description;
+          newField.group = this.group;
+          newField.icon = this.icon;
           await fs.writeFile(this.$route.params.path, JSON.stringify(newField, null, 2), 'utf8');
           this.$store.commit('addToast', { message: `“${this.customFieldName}” was saved successfully`, type: 'positive' });
           this.$store.commit('addLocallyChangedFile', this.$route.params.path);
@@ -179,6 +278,10 @@ export default {
         if (field.errors) delete field.errors; // clear empty errors properties
       }
       return true;
+    },
+    validateName() {
+      if (!this.newCustomFieldName || !this.newCustomFieldName.trim()) this.nameError = 'A name is required';
+      else this.nameError = '';
     },
   },
   mixins: [isPrivilegedUser],
@@ -298,8 +401,19 @@ export default {
       &.v-leave-to
         opacity: 0
 
-  .toggle
-    margin-top: 1.5rem
+  .icon-picker-wrapper
+    display: flex
+    align-items: center
+    margin-top: 2rem
+
+    > span
+      margin-right: 1rem
+      overflow: hidden
+      text-overflow: ellipsis
+      white-space: nowrap
+
+    > .icon-picker
+      margin-left: auto
 
   .highlight-box
     margin-top: 2rem
