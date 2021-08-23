648c50b8a06bd7a035f221813667af7329fa7a4c
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-08-23 22:08:31 +0200

    Fix nested field expansion

--- src/assets/js/expandVisualOnlyFields.js
+++ src/assets/js/expandVisualOnlyFields.js
@@ -1,3 +1,5 @@
+import { cloneDeep } from 'lodash-es';
+
 /**
  * A function to recursively expand any fields with visualOnly === true and a value
  * @param {array} fields - the array of fields to process
@@ -5,6 +7,12 @@
 export default function expandVisualOnlyFields(fields) {
   return fields.reduce((acc, field) => {
     if (field.visualOnly && Array.isArray(field.value) && field.value.length > 0) return acc.concat(expandVisualOnlyFields(field.value));
+    if (field.value && Array.isArray(field.value) && field.value.length > 0) {
+      const clone = cloneDeep(field);
+      clone.value = expandVisualOnlyFields(field.value);
+      acc.push(clone);
+      return acc;
+    }
     acc.push(field);
     return acc;
   }, []);
