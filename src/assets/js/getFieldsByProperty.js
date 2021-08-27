387b4f3f54a0567a9a171fc18d1ee3bf3b033678
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-08-27 17:52:46 +0200

    Add a function to get a field and path to it based on a predicate

---
+++ src/assets/js/getFieldsByProperty.js
@@ -0,0 +1,50 @@
+/**
+ * @typedef {object} FieldData
+ * @property {array} keypath - the path of the field in the Schema
+ * @property {array} contentpath - the path of the fields value in content
+ * @property {object} field - the instance of field in the Schema
+ */
+
+/**
+ * A function to get an array of key paths to fields within a Schema based a predicate function
+ * for example to quickly find all fields of a certain type
+ * @param {object} schema - the Schema to perform the search on
+ * @param {function} predicate - the predicate function to determine inclusion, will be passed the field and has to return true or false
+ * @returns {array} of {@link FieldData} for the fields matching predicate
+ */
+export default function getFieldsByProperty(schema, predicate) {
+  const { fields, tabs } = schema;
+  const keypath = [];
+  const contentpath = [];
+  const results = [];
+
+  function search(subfields, toplevel) {
+    for (let index = 0; index < subfields.length; index += 1) {
+      const currentField = subfields[index];
+      keypath.push(currentField.key);
+
+      if (toplevel && currentField.tab && tabs) {
+        const fieldTab = schema.tabs.find((tab) => tab.label === currentField.tab);
+        if (fieldTab && fieldTab.groupAs && !contentpath.includes(fieldTab.groupAs)) contentpath.push(fieldTab.groupAs);
+        else contentpath.pop();
+      }
+      if (!currentField.visualOnly) contentpath.push(currentField.key);
+
+      if (predicate(currentField)) {
+        results.push({
+          keypath: [...keypath], // cloning since these will keep changing as the loop progresses
+          contentpath: [...contentpath], // cloning since these will keep changing as the loop progresses
+          field: currentField,
+        });
+      }
+      if (Array.isArray(currentField.value)) {
+        search(currentField.value);
+      }
+      keypath.pop();
+      if (!currentField.visualOnly) contentpath.pop();
+    }
+  }
+
+  search(fields, true);
+  return results;
+}
