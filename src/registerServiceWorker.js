579526f2be2aa45d9953b38d9ae0979afba5e2c1
Amadeus Stadler <amadeus.stadler@gmail.com>
2021-09-29 17:20:08 +0200

    Handle service worker events

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -2,31 +2,47 @@
 
 import { register } from 'register-service-worker';
 
+import Store from './store';
+
+const runningLocally = window.location.host.includes('localhost') || window.location.host.includes('172.0.0.1');
+
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
+      if (!runningLocally) return;
       console.log(
         'App is being served from cache by a service worker.\n'
         + 'For more details, visit https://goo.gl/AFskqB',
       );
     },
     registered() {
-      console.log('Service worker has been registered.');
+      if (runningLocally) console.log('Service worker has been registered.');
     },
     cached() {
-      console.log('Content has been cached for offline use.');
+      if (runningLocally) console.log('Content has been cached for offline use.');
+      Store.commit('addToast', { message: 'Mattrbld was cached on your device and is available offline from now on', type: 'positive' });
     },
     updatefound() {
-      console.log('New content is downloading.');
+      if (runningLocally) console.log('New content is downloading.');
     },
-    updated() {
-      console.log('New content is available; please refresh.');
+    updated(registration) {
+      if (runningLocally) console.log('New content is available; please refresh.');
+      Store.commit('addToast', {
+        action() {
+          if (registration.waiting) registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+          window.location.reload(true);
+        },
+        actionLabel: 'Refresh',
+        message: 'A new version of Mattrbld is available, refresh to start using the newest verison',
+        timeout: false,
+      });
     },
     offline() {
-      console.log('No internet connection found. App is running in offline mode.');
+      if (runningLocally) console.log('No internet connection found. App is running in offline mode.');
     },
     error(error) {
-      console.error('Error during service worker registration:', error);
+      if (runningLocally) console.error('Error during service worker registration:', error);
+      Store.commit('addToast', { message: `There was an error while caching the application: ${error.message}`, type: 'error' });
     },
   });
 }
