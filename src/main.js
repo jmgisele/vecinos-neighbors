ef45d6c920056cd802b5600704491c171ab574fe
Amadeus Maximilian <amadeus.stadler@gmail.com>
2024-08-02 19:00:51 +0200

    Switch normalize with modern-normalize

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-import 'normalize.css';
+import 'modern-normalize';
 
 import './assets/styles/base.scss';
 
