550279350f04ddfd2c32a93dc09954ba8fd9fb97
Amadeus Maximilian <amadeus.stadler@gmail.com>
2025-10-12 15:47:53 +0200

    Hide initial pull toast if it exists when GitAuth is called

--- src/mixins/gitAuth.js
+++ src/mixins/gitAuth.js
@@ -19,17 +19,37 @@ export default {
         const { user, password } = this.$store.state.user.gitAuth;
         return { username: user, password };
       }
+
+      // remove the mobile on initial pull toast so it doesn’t cover modal buttons
+      // this isn't the right place to do this, but I can’t really think of a
+      // better way right now that isn’t overly complex
+      const pullToast = this.$store.state.application.toasts.find((toast) => toast.id === 'initial-pull-start');
+      if (pullToast) this.$store.commit('removeToast', 'initial-pull-start');
+
       this.gitLoginMessage = `This repository seems to be private. Please log into your <strong>${this.gitProvider || 'Git'}</strong> account to confirm that you may perform this action.`;
       this.credentials = await this.openGitLoginModal();
       this.showGitLoginModal = false;
+
+      // restore the toast
+      if (pullToast && !this.$store.state.application.sidebarVisible) this.$store.commit('addToast', pullToast);
+
       if (this.credentials === 'cancel') return { cancel: true };
       return { username: this.credentials.user, password: this.credentials.password };
     },
     async onGitAuthFailure() {
+      // remove the mobile on initial pull toast so it doesn’t cover modal buttons
+      // this isn't the right place to do this, but I can’t really think of a
+      // better way right now that isn’t overly complex
+      const pullToast = this.$store.state.application.toasts.find((toast) => toast.id === 'initial-pull-start');
+      if (pullToast) this.$store.commit('removeToast', 'initial-pull-start');
+
       if (this.$store.state.user.gitAuth) this.$store.commit('setUserProperty', { key: 'gitAuth', value: null });
       this.gitLoginMessage = 'Sorry, that didn’t work. This might mean that you don’t have access to this repository, or that you typed the wrong username / password combination. Please try again.';
       this.credentials = await this.openGitLoginModal();
       this.showGitLoginModal = false;
+
+      // restore the toast
+      if (pullToast && !this.$store.state.application.sidebarVisible) this.$store.commit('addToast', pullToast);
 
       if (this.credentials === 'cancel') return { cancel: true };
       return { username: this.credentials.user, password: this.credentials.password };
